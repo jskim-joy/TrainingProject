a9edce938d7b87df9e9c3f6ec0c72ca803b22369
H.K.KANG <110802842+vrad-hkkang@users.noreply.github.com>
2022-09-28 11:10:27 +0900

    [Sulgi]루프달기연습

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SummaryContainer from "./components/SummaryContainer.vue";
+import FilterResult from "./components/FilterResult.vue";
+import { ref } from "vue";
+
+const fontSize = ref(24);
+</script>
+
+<template>
+  <div class="background">
+    <div :style="{ 'font-size': fontSize + 'px' }">
+      <strong>선택한 술기 요약</strong>
+      <summary-container />
+      <BarChart></BarChart>
+    </div>
+    <div :style="{ 'font-size': fontSize + 'px' }">
+      <strong>필터 결과</strong>
+      <filter-result />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
