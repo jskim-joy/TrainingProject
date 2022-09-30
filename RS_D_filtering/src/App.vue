27c5e8f5a604fa99bee25a243b84adef260914a9
H.K.KANG <110802842+vrad-hkkang@users.noreply.github.com>
2022-09-30 11:27:00 +0900

    RS_D 페이지-촬영법 미선택 페이지.

---
+++ RS_D_filtering/src/App.vue
@@ -0,0 +1,17 @@
+/** *RS_D_filtering_student의 root 페이지 (최상단). */
+
+<template>
+  <section><div :style="titleFont">선택한 학생 요약</div></section>
+  <div></div>
+  <section><div :style="titleFont">필터 결과</div></section>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const titleFont = reactive({
+  fontSize: "22px",
+});
+</script>
+
+<style scoped></style>
---
+++ RS_D_filtering/src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Author: 강형근
+ * Library / Framework: Vue 3.2.38 AND Bootstrap 5.2.1.
+ *RS_D 페이지 - 촬영법 미선택, 학생으로 필터.
+ */
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+const app = createApp(App);
+app.mount("#app");
