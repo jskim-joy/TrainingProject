10608ee1023f7783acb3c5527c896c83ce00d9f2
H.K.KANG <110802842+vrad-hkkang@users.noreply.github.com>
2022-10-04 13:06:13 +0900

    현재진행상황.

--- RS_D_filtering/src/App.vue
+++ RS_D_filtering/src/App.vue
@@ -1,17 +1,71 @@
-/** *RS_D_filtering_student의 root 페이지 (최상단). */
+/** *RS_D_filtering_student의 root 페이지 (최상단). size = 360 * 720*/
 
 <template>
-  <section><div :style="titleFont">선택한 학생 요약</div></section>
-  <div></div>
-  <section><div :style="titleFont">필터 결과</div></section>
+  <main :style="mainStyle">
+    <section>
+      <div :style="titleOneFont">선택한 학생 요약</div>
+      <div :style="selectedStudentSummaryStyle">
+        <selected-student-summary />
+      </div>
+    </section>
+    <section>
+      <div :style="titleTwoFont">필터 결과</div>
+      <div class="filterResultStyle"><filter-result /></div>
+    </section>
+  </main>
 </template>
 
 <script setup>
 import { reactive } from "vue";
+import selectedStudentSummary from "./components/selectedStudentSummary.vue";
+import filterResult from "./components/filterResult.vue";
 
-const titleFont = reactive({
-  fontSize: "22px",
+const mainStyle = reactive({
+  height: "720px",
+  width: "360px",
+  border: "2px solid #ff0000",
+  backgroundColor: "#F2F2F4",
 });
-</script>
 
-<style scoped></style>
+const titleOneFont = reactive({
+  fontSize: "22px",
+  fontWeight: "700",
+  width: "152px",
+  height: "27px",
+  color: "#262626",
+  marginTop: "20px",
+});
+
+const titleTwoFont = reactive({
+  fontSize: "22px",
+  fontWeight: "700",
+  width: "86px",
+  height: "27px",
+  color: "#262626",
+  marginTop: "40px",
+});
+
+const selectedStudentSummaryStyle = reactive({
+  marginTop: "10px",
+  height: "180px",
+  border: "2px solid #00FF00",
+});
+
+// const filterResultStyle = reactive({
+//   marginTop: "10px",
+//   height: "402px",
+//   border: "2px solid #00FF00",
+//   overflow: "auto",
+// });
+</script>
+<style scoped>
+.filterResultStyle {
+  margin-top: 10px;
+  height: 402px;
+  border: 2px solid #00ff00;
+  overflow: scroll;
+}
+.filterResultStyle::-webkit-scrollbar {
+  display: none;
+}
+</style>
---
+++ RS_D_filtering/src/components/selectedStudentSummary.vue
@@ -0,0 +1,103 @@
+<template>
+  <div :style="cardStyle" @mouseover="hover = true" @mouseleave="hover = false">
+    <div v-if="hover" :style="titleStyle">반평균 성적</div>
+    <div v-else :style="titleStyle">학생명의 평균 성적</div>
+    <div :style="contentStyle">
+      <div>
+        <img :style="imgStyle" src="../assets/images/scoreState.png" />
+      </div>
+      <div :style="titleGroupStyle">
+        <div :style="groupOneStyle">
+          <span>등각촬영</span>
+          <span>평행촬영</span>
+        </div>
+        <div v-if="hover" :style="scoreOneStyle">
+          <span>1점</span>
+          <span>1점</span>
+        </div>
+        <div v-else :style="scoreOneStyle">
+          <span>00점</span>
+          <span>00점</span>
+        </div>
+        <div :style="groupTwoStyle">
+          <span>교익촬영</span>
+          <span>교합촬영</span>
+        </div>
+        <div v-if="hover" :style="scoreTwoStyle">
+          <span>2점</span>
+          <span>2점</span>
+        </div>
+        <div v-else :style="scoreTwoStyle">
+          <span>00점</span>
+          <span>00점</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+
+const hover = ref(false);
+
+const cardStyle = reactive({
+  width: "328px",
+  height: "155px",
+  border: "1px solid #000000 ",
+  background: "#FFFFFF",
+  borderRadius: "16px",
+});
+
+const titleStyle = reactive({
+  width: "133px",
+  height: "21px",
+  color: "#262626",
+  marginTop: "15px",
+  marginLeft: "20px",
+  fontSize: "17px",
+  fontWeight: "700",
+});
+
+const contentStyle = reactive({
+  display: "flex",
+  marginTop: "10px",
+  justifyContent: "space-evenly",
+  marginLeft: "-39px",
+});
+
+const imgStyle = reactive({
+  marginLeft: "39px",
+  marginBottom: "15px",
+});
+
+const titleGroupStyle = reactive({
+  width: "118px",
+  height: "94px",
+  border: "1px solid #000000",
+});
+
+const groupOneStyle = reactive({
+  display: "flex",
+  justifyContent: "space-evenly",
+  fontSize: "12px",
+  border: "1px solid #000000",
+});
+
+const scoreOneStyle = reactive({
+  display: "flex",
+  justifyContent: "space-evenly",
+});
+
+const groupTwoStyle = reactive({
+  display: "flex",
+  justifyContent: "space-evenly",
+  fontSize: "12px",
+  border: "1px solid #000000",
+});
+
+const scoreTwoStyle = reactive({
+  display: "flex",
+  justifyContent: "space-evenly",
+});
+</script>
